827d8c97bb915b8d4540959e7d51c715858c60cc
Steven Garberg <steven.garberg.jr@gmail.com>
2021-09-22 13:42:22 -0400

    Added NullableIsoDateConverter

---
+++ JsonConverters/NullableIsoDateConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace JsonConverters
+{
+    public class NullableIsoDateConverter : JsonConverter<DateTime?>
+    {
+        public override DateTime? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            bool wasParsed = DateTime.TryParse(reader.GetString(), out DateTime value);
+            return wasParsed ? value : (DateTime?)null;
+        }
+
+        public override void Write(Utf8JsonWriter writer, DateTime? value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(value?.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture));
+        }
+    }
+}
