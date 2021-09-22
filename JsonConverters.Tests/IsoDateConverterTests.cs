b4df2919931ab414c49407693b3e4d3cdb0d59fb
Steven Garberg <steven.garberg.jr@gmail.com>
2021-09-22 13:52:10 -0400

    Added unit tests for IsoConverters

---
+++ JsonConverters.Tests/IsoDateConverterTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.Json;
+using Xunit;
+
+namespace JsonConverters.Tests
+{
+    public class IsoDateConverterTests
+    {
+        private readonly IsoDateConverter _converter = new IsoDateConverter();
+
+        [Fact]
+        public void Read_Returns_Datetime()
+        {
+            DateTime dateTime = DateTime.UtcNow;
+            byte[] bytes = JsonSerializer.SerializeToUtf8Bytes(dateTime);
+
+            Utf8JsonReader reader = new Utf8JsonReader(bytes);
+            reader.Read();
+            DateTime output = _converter.Read(ref reader, typeof(DateTime), null);
+            
+            Assert.Equal(dateTime.Date, output.Date);
+        }
+    }
+}
---
+++ JsonConverters.Tests/NullableIsoDateConverterTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text.Json;
+using Xunit;
+
+namespace JsonConverters.Tests
+{
+    public class NullableIsoDateConverterTests
+    {
+        private readonly NullableIsoDateConverter _converter = new NullableIsoDateConverter();
+
+        [Fact]
+        public void Read_Returns_NullableDatetime_WhenHasValue()
+        {
+            DateTime? dateTime = DateTime.UtcNow;
+            byte[] bytes = JsonSerializer.SerializeToUtf8Bytes(dateTime);
+
+            Utf8JsonReader reader = new Utf8JsonReader(bytes);
+            reader.Read();
+            DateTime? output = _converter.Read(ref reader, typeof(DateTime?), null);
+            
+            Assert.NotNull(output);
+            Assert.Equal(dateTime.Value.Date, output.Value.Date);
+        }
+        
+        [Fact]
+        public void Read_Returns_Null_WhenNotHasValue()
+        {
+            DateTime? dateTime = null;
+            byte[] bytes = JsonSerializer.SerializeToUtf8Bytes(dateTime);
+
+            Utf8JsonReader reader = new Utf8JsonReader(bytes);
+            reader.Read();
+            DateTime? output = _converter.Read(ref reader, typeof(DateTime?), null);
+            
+            Assert.Null(output);
+        }
+    }
+}
